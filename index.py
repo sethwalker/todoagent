2d03b33424c3a28f1f7737c4aebe56a58af2e1fd
Seth Walker <seth.h.walker@gmail.com>
2024-04-04 21:45:47 -0400

    creates schema (sometimes unnecessarily), does not seem to write the row tho it says it did

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
+from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.agent_toolkits import create_sql_agent
+from langchain.agents import AgentExecutor
+from langchain.agents import create_openai_functions_agent
+from langchain_experimental.tools import PythonREPLTool
+
+
+tools = [PythonREPLTool()]
 
 
 # requires OPENAI_API_KEY env var to be set
@@ -12,46 +18,29 @@ db = SQLDatabase.from_uri("sqlite:///agent.db")
 
 # ).partial(dialect=db.dialect, dbcontext=db.get_context(), top_k=3)
 
-agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
+instructions = """You are an agent designed to write and execute python code to power a todo app.
+You have access to a python REPL, which you can use to execute python code.
+There is a SQLite database addressable at the uri "sqlite:///agent.db" that you can connect to
+and run any valid SQL, including creating and altering tables. You should get the existing schema before you
+decide how to interact with it.
+If you get an error, debug your code and try again.
+Write comments in the code and in the SQL statements to explain your thinking.
+Query the database after taking action to confirm that the expected change has occurred.
 
-agent_executor.invoke(
-    """
-    You have permission to create and alter tables.
-    Execute any necessary SQL to accomplish this task in the context of a todo app,
-    including creating or altering tables.
-    Break queries down into subqueries if appropriate.
-    Add comments to all queries to explain what they are doing.
-    Write and execute a final query that confirms that the expected change has happened.
+If it does not seem like you can write code to perform the task, explain why not.
+"""
+base_prompt = hub.pull("langchain-ai/openai-functions-template")
+prompt = base_prompt.partial(instructions=instructions)
 
+insert_task = """
     <task>
     remind me to show this to alix asap
     </task>
-    """
-)
 
-output_parser = StrOutputParser()
+"""
 
-exit()
+agent = create_openai_functions_agent(ChatOpenAI(temperature=0), tools, prompt)
 
-chain = prompt | llm | output_parser
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-chain.get_prompts()[0].pretty_print()
-
-# todo: combine this with the following query or predetermine the schema. Currently bombs on the first run
-# through if the generated query doesn't match the generated schema
-res = chain.invoke(
-    {"query": "create a table to store todo items if it does not already exist"}
-)
-print("schema: ", res)
-
-# db.run(res)
-
-res = chain.invoke({"query": "remind me to buy the milk before next tuesday"})
-print("insert: ", res)
-
-db.run(res)
-
-res = chain.invoke({"query": "what's on my todo list?"})
-
-todos = db.run(res)
-print(todos)
+agent_executor.invoke({"input": "perform the following task %s" % insert_task})
